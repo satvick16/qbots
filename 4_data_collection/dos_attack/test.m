974c096b69c143c01bc0513fd6da36c79c14d541
Satvick Acharya <satvickacharya16@gmail.com>
2022-07-28 13:47:53 -0400

    set up data collection for distracted driver

--- 4_data_collection/dos_attack/test.m
+++ 4_data_collection/dos_attack/test.m
@@ -1,1 +1,6 @@
-plot(ans(1, :), ans(2, :)); hold on; plot(ans(1, :), ans(3, :)); hold on; plot(ans(1, :), ans(4, :)); hold on; plot(ans(1, :), ans(5, :)); legend();
+plot(ans(1, :), ans(2, :)); 
+hold on; 
+plot(ans(1, :), ans(3, :)); 
+plot(ans(1, :), ans(4, :)); 
+plot(ans(1, :), ans(5, :)); 
+legend('autonomous 1', 'human 1', 'human 2', 'autonomous 2');
