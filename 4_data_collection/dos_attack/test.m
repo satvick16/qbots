51d27c5409c59dade6eab44e06c06aa0bcc65c89
Satvick Acharya <satvickacharya16@gmail.com>
2022-08-16 15:33:19 -0400

    completed more data collection

--- 4_data_collection/dos_attack/test.m
+++
@@ -1,6 +0,0 @@
-plot(ans(1, :), ans(2, :)); 
-hold on; 
-plot(ans(1, :), ans(3, :)); 
-plot(ans(1, :), ans(4, :)); 
-plot(ans(1, :), ans(5, :)); 
-legend('autonomous 1', 'human 1', 'human 2', 'autonomous 2');
